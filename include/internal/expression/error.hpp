c5f568a457b4a5e1f1494ca2c06899d2a0608838
mrd <mrdream0426@outlook.com>
2026-07-30 17:26:12 +0800

    refactor(expression): expession 内所有 error 继承同一父类 ExpressionError

--- include/internal/expression/error.hpp
+++ include/internal/expression/error.hpp
@@ -1,9 +1,13 @@
 #include <stdexcept>
 #include <string>
 
-class LexerError: public std::runtime_error{
+class ExpressionError: public std::runtime_error{
 public:
-    LexerError(const std::string &message, const std::string_view &pattern, unsigned int pos);
+    ExpressionError(
+        const std::string &message,
+        const std::string_view &pattern, 
+        unsigned int pos, 
+        const std::string &prefix="Expression Error");
 
     const char *what() const noexcept override{return full_message_.c_str();};
 private:
@@ -11,4 +15,14 @@ private:
     std::string pattern_;
     unsigned pos_;
     std::string full_message_;
+};
+
+
+class LexerError: public ExpressionError{
+public:
+    LexerError(
+        const std::string &message,
+        const std::string_view &pattern, 
+        unsigned int pos):
+        ExpressionError(message, pattern, pos, "Lexer Error"){};
 };
